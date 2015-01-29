8c44375fda9de26748a66602fdfa0e202ec9d341
Benjamin Herson & Gabe Finch <student@epicodus.com>
2015-01-28 16:18:15 -0800

    add pages/connectivity for questions/surveys

--- app.rb
+++ app.rb
@@ -44,3 +44,51 @@ delete('/survey_delete') do
   @survey.destroy()
   redirect('/')
 end
+
+get ('/edit_question/:question_id/:survey_id') do
+  @question = Question.find(params['question_id'].to_i)
+  @survey = Survey.find(params['survey_id'].to_i)
+  erb(:edit_question)
+end
+
+patch('/question_update') do
+  new_ask = params.fetch("new_ask")
+  question_id = params.fetch("question_id").to_i()
+  @question = Question.find(question_id)
+  @question.update({:ask => new_ask})
+  redirect('/')
+end
+
+delete('/question_seperate') do
+  question = Question.find(params['question_id'].to_i)
+  survey = Survey.find(params['survey_id'].to_i)
+  survey.questions.delete(question)
+  redirect('/')
+end
+
+delete('/question_delete') do
+  quest = Question.find(params.fetch("question_id").to_i())
+  quest.destroy()
+  redirect('/')
+end
+
+get('/questions') do
+  @questions = Question.all()
+  @surveys = Survey.all()
+  erb(:questions)
+end
+
+post('/new_question') do
+  ask = params.fetch("ask")
+  question = Question.create({:ask => ask})
+  redirect('/questions')
+end
+
+post('/choose_question') do
+  survey_to_modify = Survey.find(params['survey_id'])
+  params['question_ids'].each do |id|
+    ques = Question.find(id.to_i())
+    survey_to_modify.questions.push(ques)
+  end
+  redirect ('/')
+end
